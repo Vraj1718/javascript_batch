911e0560dca033f97d9163fb50b7f9466e60a0ea
Vraj1718 <rajprajapati1410@gmail.com>
2024-08-12 18:06:51 +0530

    update

--- datatype.js
+++ datatype.js
@@ -1,3 +1,57 @@
-let a=12;
+// number 
+
+let a=25;
 console.log(typeof a);
+console.log(a)
+
+//string
+
+let s_name="raj prajapati@1718"
+console.log(typeof s_name);
+console.log(s_name)
+
+//boolean
+let islogin=true
+console.log(typeof islogin);
+console.log(islogin)
+
+let isfaild=false
+console.log(isfaild)
+
+//undefined
+
+let data2;
+console.log(typeof data2)
+
+//null
+
+let data1=null;
+let data3;
+console.log(typeof data1)
+
+//object
+
+let user = {
+    name: "Raj",
+    address: "palanpur",
+    age: 21,
+    isLogin: true,
+  };
+
+ console.log(typeof user);
+ console.log(user);
+ console.log(user.address); 
+ console.log(user.age);
+
+// bigint
+
+let num=12345678901234567890123456789n
+console.log(typeof num);
+console.log(num)
+
+//array
+ let arr=[1,"raj",true,2.35,"gaurang",564n]
+ console.log(typeof arr)
+console.log(arr)
+console.log(arr[4])
 
