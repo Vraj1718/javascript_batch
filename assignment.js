801e0b5f5def1c69e511ca34bfa2bf709b74af0f
Vraj1718 <rajprajapati1410@gmail.com>
2024-08-20 21:44:55 +0530

    uptate

---
+++ assignment.js
@@ -0,0 +1,74 @@
+// print 1.100 using while dowhile and for
+// print same 100.1 using while dowhile and for
+// print odd number between 1.100
+// print even number between 1.100
+// print numbers between 1.100 which are divisible by 5 and 3
+// print numbers between 1.100 which are divisible by 5 and 3 and 7
+
+
+
+// print 1.100 using while dowhile and for
+let a=1;
+
+while(a<=100){
+    console.log(a);
+    a++;
+}
+
+console.log();
+
+// print same 100.1 using while dowhile and for
+let b=1;
+
+do{
+    console.log(b);
+    b++;
+}while(b<=100)
+
+
+// print odd number between 1.100
+
+// console.log();
+// let x=1;
+
+// while(x<=100){
+//     console.log(x);
+//     x+=2;
+// }
+
+// print odd number between 1.100
+console.log();
+
+for (let i = 1; i <= 100; i++) {
+    if (i % 2 !== 0) {
+        console.log(i);
+    }
+}
+
+// print even number between 1.100
+console.log();
+
+for (let i = 1; i <= 100; i++) {
+    if (i % 2 == 0) {
+        console.log(i);
+    }
+}
+
+console.log();
+// print numbers between 1.100 which are divisible by 5 and 3
+
+for(i=1;i<=100;i++){
+    if(i%5 == 0 && i%3 ==0){
+        console.log(i);
+    }
+}
+
+console.log();
+//print numbers between 1.100 which are divisible by 5 and 3 and 7
+
+for(i=1;i<=100;i++){
+    if(i%5 == 0 && i%3 ==0 && i%7 == 0){
+        console.log(i);
+    }
+}
+
---
+++ qna.js
@@ -0,0 +1,40 @@
+//1 What is the result of the expression 45 % 6?
+
+console.log(45%6)
+
+//2    Calculate the result of 5645 % 10. What does this operation represent?
+
+console.log(5645%10)
+
+//3  What is the output of ~~(1234 / 10)? Explain the use of the ~~ operator.
+
+console.log(~~(1234/10))
+
+//4     Evaluate 2 ** 3. What mathematical operation does this represent?
+
+console.log(2**3)
+
+//5   Compare the values of n1 and n2 using the != operator. What is the output?
+
+let n1=10;
+let n2=20;
+console.log(n1!=n2)
+
+//6   Determine the result of the logical expression 12 <= 12 && 12 > 3 && 12 < 3.
+console.log(12 <= 12 && 12 > 3 && 12 < 3)
+
+console.log(12 <= 12 || 12 > 3 || 12 < 3)
+//7 If num starts at 34, what will be its value after executing num += 10 and then num %= 2?
+
+num=34;
+console.log(num)
+num+=10; //44
+console.log(num)
+num%=2; //100
+console.log(num)
+
+//8    What is the final value of k after executing k-- if k initially equals 34?
+
+k=34;
+k--;
+console.log(k)
